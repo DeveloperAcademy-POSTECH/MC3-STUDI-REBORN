7f6e91d613353af3645b5bc67a4e2a30832a4638
namgeon1106 <namgeon1106@naver.com>
2022-07-18 20:42:56 +0900

    refactor: view controller에 background 적용

--- Reborn/AnimalListViewController/AnimalListViewController.swift
+++ Reborn/AnimalListViewController/AnimalListViewController.swift
@@ -10,5 +10,6 @@ import UIKit
 class AnimalListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
     }
 }
