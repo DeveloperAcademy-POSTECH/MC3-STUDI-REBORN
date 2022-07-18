cd2dcfe2b3cd0e7c81ab110735a69618003b0baa
namgeon1106 <namgeon1106@naver.com>
2022-07-18 18:54:46 +0900

    feat: AnimalListViewController 추가 및 root view controller로 설정 [#5]

---
+++ Reborn/AnimalListViewController/AnimalListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AnimalListViewController.swift
+//  Reborn
+//
+//  Created by 김남건 on 2022/07/18.
+//
+
+import UIKit
+
+class AnimalListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Reborn/SceneDelegate.swift
+++ Reborn/SceneDelegate.swift
@@ -18,7 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = UINavigationController(rootViewController: AnimalListViewController())
         window?.makeKeyAndVisible()
     }
 
