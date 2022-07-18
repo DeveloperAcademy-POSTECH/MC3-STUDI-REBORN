57446d5f46d7ff30f931801e2de50e8ee91cb3ff
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2022-07-18 20:53:09 +0900

    refactor: 쿼리 파리미터 변수명 수정
    Constants의 AnimalApi에 있는 쿼리 파라미터의 변수명을 수정했습니다.

--- Reborn/Helpers/Constants.swift
+++ Reborn/Helpers/Constants.swift
@@ -12,7 +12,7 @@ import Foundation
 public enum AnimalApi {
     static let requestUrl = "http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?_type=json"
     static let key = "serviceKey=n1TP%2BU609SiYa5OWk03fYi1r4ASh04%2F2g54Hixw8os6IIzkhypv3doFp8CT2m4s9jfgTm%2F1G%2BPspd3XmzkbDcw%3D%3D"
-    static let state = "state=notice"
-    static let pageNumber = "pageNo=1"
-    static let numberOfRows = "numOfRows=20"
+    static let stateQuery = "state=notice"
+    static let pageNumberQuery = "pageNo=1"
+    static let numberOfRowsQuery = "numOfRows=20"
 }
