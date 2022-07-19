4d96a794f869ac4078ad3f4620b9410447a59b1c
namgeon1106 <namgeon1106@naver.com>
2022-07-19 20:11:40 +0900

    feat: cell에 photoView 추가[#6]

--- Reborn/AnimalListViewController/AnimalInfoCell.swift
+++ Reborn/AnimalListViewController/AnimalInfoCell.swift
@@ -11,6 +11,23 @@ final class AnimalInfoCell: UITableViewCell {
     static let reuseID = "AnimalInfoCell"
     static let rowHeight: CGFloat = 172
     
+    private let photoView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 140),
+            imageView.heightAnchor.constraint(equalToConstant: 140)
+        ])
+        
+        imageView.layer.cornerRadius = 14
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .gray
+        
+        return imageView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
@@ -22,6 +39,10 @@ final class AnimalInfoCell: UITableViewCell {
     }
     
     private func setup() {
-        
+        contentView.addSubview(photoView)
+        NSLayoutConstraint.activate([
+            photoView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            photoView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20)
+        ])
     }
 }
