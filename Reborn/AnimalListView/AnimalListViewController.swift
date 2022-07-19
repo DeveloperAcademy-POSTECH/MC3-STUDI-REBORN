9e7a07c5f23a165ccde38ddc99e11c65583eed65
rkddnr330 <rkddnr330@gmail.com>
2022-07-19 12:54:52 +0900

    feat: UITableViewCell UI 작업
    로컬에서 작업했던 UITableViewCell에 약간의 리팩토링 과정을 거쳤습니다
    
    - Asset에 임시 이미지 (cat1) 추가
    - 이미지, 공고 기한 레이블, 품종, 성별 나이, 보호소 위치, 하트 이미지

--- Reborn/AnimalListView/AnimalListViewController.swift
+++ Reborn/AnimalListView/AnimalListViewController.swift
@@ -6,33 +6,58 @@
 //
 
 import UIKit
+// TODO: private 체크
 
 // MARK: - AnimalListViewController
 class AnimalListViewController: UIViewController {
+
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
 
+        setupTableView()
+    }
+    
+    // MARK: - setupTableView
+    func setupTableView() {
+        tableView = UITableView(frame: .zero)
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //tableView의 constraint 설정
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+        
+        //AnimalInfoCell 등록
+        tableView.register(AnimalInfoCell.self, forCellReuseIdentifier: AnimalInfoCell.identifier)
     }
 }
 
 // MARK: - UITableViewDataSource 프로토콜
 extension AnimalListViewController: UITableViewDataSource {
     
-    // MARK: - cell 개수
+    //cell 개수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
-    // MARK: - cell 데이터
+    //cell 데이터
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: AnimalInfoCell.identifier, for: indexPath) as? AnimalInfoCell else { return UITableViewCell() }
 
         return cell
     }
     
-    // MARK: - cell 높이
+    //cell 높이
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 172
     }
