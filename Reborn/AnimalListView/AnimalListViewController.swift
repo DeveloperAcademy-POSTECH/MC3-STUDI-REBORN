f70b755267a42a47f88630584f9518d7bbf062a6
rkddnr330 <rkddnr330@gmail.com>
2022-07-20 15:34:37 +0900

    feat: AnimalListViewController의 NavigationBar 지역선택 구현
    AnimalListViewController에 있는 NavigationBar의 지역선택을 구현했습니다.
    
    - 지역 선택 UIMenu로 구현 (지역 목록 추가 작업은 추후 예정)
    - 구현 과정 : UIStack -> UIMenu & UIButton -> UIBarButtonItem

--- Reborn/AnimalListView/AnimalListViewController.swift
+++ Reborn/AnimalListView/AnimalListViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
-// TODO: private 체크
 
 // MARK: - AnimalListViewController
 final class AnimalListViewController: UIViewController {
 
     private var tableView: UITableView!
+    
+    // MARK: - 지역선택 변수
+    private var locationText: BaseLabel = {
+        let label = BaseLabel()
+        label.text = "서울특별시"
+        
+        return label
+    }()
+    
+    private var locationSymbol: UIImageView = {
+        let label = UIImageView()
+        label.image = UIImage(systemName: "chevron.down")
+        label.tintColor = .black
+        
+        return label
+    }()
 
+    private var menuItems: [UIAction] {
+        return [
+            UIAction(title: "서울특별시", handler: { _ in self.locationText.text = "서울특별시" }),
+            UIAction(title: "부산광역시", handler: { _ in self.locationText.text = "부산광역시" })]
+    }
+
+    private var locationMenu: UIMenu {
+        return UIMenu(children: menuItems)
+    }
+    
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -45,6 +71,7 @@ final class AnimalListViewController: UIViewController {
     // MARK: - setupNavBarButton
     private func setupNavBarButton() {
         //NavigationBarButtonItem 구성
+        navigationItem.leftBarButtonItem = generateButtonItemFromStack()
         navigationItem.rightBarButtonItems = [
             UIBarButtonItem(image: UIImage(systemName: "slider.horizontal.3"), style: .plain, target: self, action: #selector(goToFilter)),
             UIBarButtonItem(image: UIImage(systemName: "heart"), style: .plain, target: self, action: #selector(goToLike))
@@ -57,7 +84,49 @@ final class AnimalListViewController: UIViewController {
         })
     }
     
-    //관심목록 이동 함수
+    // MARK: - 지역선택 함수
+    private func generateButtonItemFromStack() -> UIBarButtonItem {
+        // 1. Stack -> Button
+        let button = UIButton()
+        let stack = generateLocationStack()
+        button.addSubview(stack)
+        
+        // 1-2. Button, Stack의 AutoLayout 조정 (Button의 영역을 Stack만큼 준다)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: stack.topAnchor),
+            button.leadingAnchor.constraint(equalTo: stack.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: stack.trailingAnchor),
+            button.bottomAnchor.constraint(equalTo: stack.bottomAnchor)
+        ])
+        
+        // 1-3. Button에 menu 추가
+        if #available(iOS 14.0, *) {
+            button.menu = locationMenu
+            button.showsMenuAsPrimaryAction = true
+        }
+        
+        // 2. Button -> BarButtonItem
+        let buttonItem = UIBarButtonItem()
+        buttonItem.customView = button
+        
+        return buttonItem
+    }
+    
+    private func generateLocationStack() -> UIStackView {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.alignment = .center
+        stack.spacing = 6
+        
+        stack.addArrangedSubview(locationText)
+        stack.addArrangedSubview(locationSymbol)
+        
+        return stack
+    }
+
+    // MARK: - 관심목록 이동 함수
     @objc private func goToLike() {
         if let from = navigationController {
             let destination = LikeDemoViewController()
@@ -65,7 +134,7 @@ final class AnimalListViewController: UIViewController {
         }
     }
     
-    //필터 모달 구현 함수
+    // MARK: - 필터 모달 구현 함수
     @objc private func goToFilter() {
         let destination = FilterDemoViewController()
         present(destination, animated: true, completion: nil)
