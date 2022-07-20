cc7bb496ee43f0c9f3fdbd45dc57b926fc95685f
Hyeminchio <103024799+Hyeminchio@users.noreply.github.com>
2022-07-20 14:53:01 +0900

    feat: LikeListView UI구현

---
+++ Reborn/Models/LikeCollectionViewCell.swift
@@ -0,0 +1,101 @@
+//
+//  LikeCollectionViewCell.swift
+//  Reborn
+//
+//  Created by Hyemin Choi on 2022/07/19.
+//
+
+import UIKit
+
+class LikeCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "LikeCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setImageViewConstraints()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+            heartImageView.isUserInteractionEnabled = true
+            heartImageView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    
+    
+//    하트버튼
+    private let heartImageView: UIImageView = {
+        let image = UIImage(systemName: "heart.fill")
+        let imageView = UIImageView(image: image)
+        imageView.clipsToBounds = true
+        imageView.tintColor = UIColor(named: "CRed")
+        return imageView
+    }()
+    
+    
+
+//    이미지
+    private let animalImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "1.png")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 14
+        return imageView
+    }()
+    
+//    레이블
+    private let animalLabel: UILabel = {
+        let label = UILabel()
+        label.text = "공고 종료 1일 전"
+        label.backgroundColor = UIColor(named: "CRed")
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.textAlignment = .center
+        label.layer.cornerRadius = 12
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setImageViewConstraints() {
+        self.addSubview(animalImageView)
+        animalImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            animalImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            animalImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            animalImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            animalImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        self.addSubview(animalLabel)
+        animalLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            animalLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 27),
+            animalLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            animalLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.6),
+            animalLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.1)
+        ])
+        
+        self.addSubview(heartImageView)
+        heartImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            heartImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            heartImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+
+        // Your action
+    }
+}
+
+
+
+
---
+++ Reborn/Models/LikeListViewController.swift
@@ -0,0 +1,95 @@
+//
+//  LikeListViewController.swift
+//  Reborn
+//
+//  Created by Hyemin Choi on 2022/07/19.
+//
+
+import UIKit
+
+final class LikeListViewController: UIViewController {
+
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+    
+    private let flowLayout = UICollectionViewFlowLayout()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        
+        label.frame = CGRect(x: 20, y: 108, width: 350, height: 66)
+        label.textColor = .cBlack
+        label.backgroundColor = .cBeige
+        label.text = "🐶  공고 종료된 아이들은 \n자동으로 목록에서 사라져요"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 14
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setNavBar()
+        setBanner()
+        setCollectionView()
+        setCollectionViewConstraints()
+    }
+    
+    func setNavBar() {
+        navigationItem.title = "관심 목록"
+        navigationController?.navigationBar.tintColor = .cBlack
+    }
+    
+    func setBanner() {
+        view.addSubview(label)
+    }
+    
+    func setCollectionView() {
+        collectionView.dataSource = self
+        
+        flowLayout.scrollDirection = .vertical
+        flowLayout.itemSize = CGSize(width: 165, height: 210)
+        flowLayout.sectionInset = .init(top: 20, left: 20, bottom: 20, right: 20)
+        flowLayout.minimumInteritemSpacing = 20
+        flowLayout.minimumLineSpacing = 16
+        
+        collectionView.register(LikeCollectionViewCell.self, forCellWithReuseIdentifier: LikeCollectionViewCell.identifier)
+    }
+    
+    func setCollectionViewConstraints(){
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 197).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+}
+//class heartButton : UIButton {
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//
+//        self.setTitle("button", for: .normal)
+//        self.backgroundColor = .red
+//    }
+//}
+
+
+extension LikeListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LikeCollectionViewCell.identifier, for: indexPath) as! LikeCollectionViewCell
+
+        return cell
+    }
+}
