d10fed9540223c2ab19bce04b0493ccaf689d5aa
rkddnr330 <rkddnr330@gmail.com>
2022-07-26 11:39:10 +0900

    feat: API 연동, Refresh 확인, 지역 변경 시 스크롤 위치 초기화
    - API를 연동해서 Refresh 기능 작동을 확인했습니다.
    - 지역 변경 시, 스크롤 위치가 맨 위로 가도록 했습니다.
    - Info.plist에 항목을 추가하여 HTTP block을 수정했습니다.

--- Reborn/AnimalListViewController/AnimalListViewController.swift
+++ Reborn/AnimalListViewController/AnimalListViewController.swift
@@ -16,19 +16,35 @@ final class CustomButton: UIButton {
 }
 
 final class AnimalListViewController: UIViewController {
+    
+    // MARK: - 네트워크 매니저
+    
+    private var networkManager = NetworkManager.shared
+    private var regionQuery = Region.none.query
+    private var pageNumberQuery = 1
+    private var fetchMore = false
+    
+    // MARK: - 유기동물 데이터 배열
+    
+    private var animals: [Item] = []
+    
     private let locationLabel: BaseLabel = {
         let label = BaseLabel(size: 20, weight: .semibold)
-        label.text = "서울특별시"
+        label.text = "전국"
         
         return label
     }()
     
     var locationMenu: UIMenu {
-        let locations = ["서울특별시", "대전광역시", "부산광역시", "세종특별자치시", "충정북도", "충청남도", "경상북도", "경상남도", "전라북도", "전라남도", "강원도", "경기도", "제주특별자치도"]
+        let locations = ["전국", "서울특별시", "대전광역시", "부산광역시", "세종특별자치시", "충청북도", "충청남도", "경상북도", "경상남도", "전라북도", "전라남도", "강원도", "경기도", "제주특별자치도"]
         var actions = [UIAction]()
         
         for location in locations {
-            actions.append(UIAction(title: location) { _ in self.changeLocation(to: location) })
+            actions.append(UIAction(title: location) { _ in
+                self.changeLocation(to: location)
+                self.makeRegionQuery(location)
+                self.setDatas(by: self.regionQuery)
+            })
         }
         
         return UIMenu(children: actions)
@@ -99,6 +115,7 @@ final class AnimalListViewController: UIViewController {
         }
     }
     
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -106,6 +123,7 @@ final class AnimalListViewController: UIViewController {
         configureNavigationBar()
         setupTableView()
         
+        setDatas()
         //당겨서 새로 고침
         initRefresh()
     }
@@ -183,6 +201,88 @@ extension AnimalListViewController {
         
         // TODO: 지역 변경 시 추가 로직 구현
     }
+    
+    // MARK: - fetch 함수들
+    
+    // 초기 세팅
+    private func setDatas() {
+        networkManager.fetchAnimal { result in
+            switch result {
+            case .success(let animalDatas):
+                self.animals = animalDatas
+                // 데이터 받아온 후 메인 쓰레드에서 테이블 뷰 리로드
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    // 지역에 맞는 데이터
+    private func setDatas(by region: String) {
+        networkManager.fetchAnimal(regionQuery: region) { result in
+            switch result {
+            case .success(let animalDatas):
+                self.animals = animalDatas
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    //스크롤 맨 위로 이동
+                    self.scrollToTop()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    // MARK: - 스크롤 맨 위로 이동하는 함수
+    
+    private func scrollToTop() {
+        let topRow = IndexPath(row: 0, section: 0)
+        self.tableView.scrollToRow(at: topRow, at: .top, animated: false)
+    }
+    
+    // MARK: - 지역 문자열로 regionQuery에 할당
+    
+    private func makeRegionQuery(_ string: String) {
+        if string == "전국" {
+            regionQuery = Region.none.query
+        } else if string == "서울특별시" {
+            regionQuery = Region.seoul.query
+        } else if string == "부산광역시" {
+            regionQuery = Region.busan.query
+        } else if string == "대구광역시" {
+            regionQuery = Region.daegu.query
+        }  else if string == "인천광역시" {
+            regionQuery = Region.incheon.query
+        } else if string == "광주광역시" {
+            regionQuery = Region.gwangju.query
+        } else if string == "대전광역시" {
+            regionQuery = Region.daejeon.query
+        } else if string == "울산광역시" {
+            regionQuery = Region.ulsan.query
+        } else if string == "경기도" {
+            regionQuery = Region.gyeonggi.query
+        } else if string == "강원도" {
+            regionQuery = Region.gangwon.query
+        } else if string == "충청북도" {
+            regionQuery = Region.choongbook.query
+        } else if string == "충청남도" {
+            regionQuery = Region.choongnam.query
+        } else if string == "전라북도" {
+            regionQuery = Region.jeonbook.query
+        } else if string == "전라남도" {
+            regionQuery = Region.jeonnam.query
+        } else if string == "경상북도" {
+            regionQuery = Region.gyeongbook.query
+        } else if string == "경상남도" {
+            regionQuery = Region.gyeongnam.query
+        } else if string == "제주특별자치도" {
+            regionQuery = Region.jeju.query
+        }
+    }
 }
 
 // MARK: - likeItem
@@ -209,13 +309,18 @@ extension AnimalListViewController {
 
 extension AnimalListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return animals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: AnimalInfoCell.reuseID, for: indexPath) as! AnimalInfoCell
+            
+        //cell에 데이터 전달
+        cell.imageUrl = animals[indexPath.row].detailImage
+        cell.animal = animals[indexPath.row]
+        
+        cell.selectionStyle = .none
         
         return cell
     }
 }
-
