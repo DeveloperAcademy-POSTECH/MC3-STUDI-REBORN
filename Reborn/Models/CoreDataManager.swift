99ad783106cbb8ad62c611057c063b59e5eafc72
namgeon1106 <namgeon1106@naver.com>
2022-07-27 17:10:00 +0900

    feat: LikedAnimal 저장 메서드, 모든 entity 받는 메서드 구현 [#39]

--- Reborn/Models/CoreDataManager.swift
+++ Reborn/Models/CoreDataManager.swift
@@ -11,6 +11,9 @@ import CoreData
 class CoreDataManager {
     static let shared = CoreDataManager()
     let persistantContainer: NSPersistentContainer
+    var viewContext: NSManagedObjectContext {
+        persistantContainer.viewContext
+    }
     
     private init() {
         persistantContainer = NSPersistentContainer(name: "AnimalModel")
@@ -18,6 +21,53 @@ class CoreDataManager {
             if let error = error {
                 fatalError("Core Data Store failed to initialize \(error.localizedDescription)")
             }
+        }
+    }
+    
+    func save() {
+        do {
+            try viewContext.save()
+            print("saved")
+        } catch {
+            viewContext.rollback()
+            print("failed to save")
+        }
+    }
+    
+    func saveAnimal(_ item: Item) {
+        let likedAnimal = LikedAnimal(context: viewContext)
+        
+        likedAnimal.id = item.id
+        
+        likedAnimal.detailImage = item.detailImage
+        likedAnimal.noticeNumber = item.noticeNumber
+        likedAnimal.noticeStartDate = item.noticeStartDate
+        likedAnimal.noticeEndDate = item.noticeEndDate
+        
+        likedAnimal.kind = item.kind
+        likedAnimal.color = item.color
+        likedAnimal.birth = item.birth
+        likedAnimal.sexCd = item.sexCd
+        likedAnimal.neutralizationStatus = item.neutralizationStatus
+        likedAnimal.weight = item.weight
+        likedAnimal.descript = item.description
+        likedAnimal.discoveredPlace = item.discoverdPlace
+        
+        likedAnimal.shelterName = item.shelterName
+        likedAnimal.shelterAddress = item.shelterAddress
+        likedAnimal.telNumber = item.telNumber
+        
+        save()
+    }
+    
+    func getAllLikedAnimals() -> [LikedAnimal] {
+        let fetchRequest = LikedAnimal.fetchRequest()
+        
+        do {
+            return try viewContext.fetch(fetchRequest)
+        } catch {
+            print("failed to fetch liked animals")
+            return []
         }
     }
 }
